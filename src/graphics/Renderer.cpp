3652fb07da14419f9eab4b44db39f2b942ef7a54
CALDERON GUILLAUME p2205143 <guillaume.calderon@etu.univ-lyon1.fr>
2023-11-01 12:46:10 +0100

    Added some rendering to the game to debug states

--- src/graphics/Renderer.cpp
+++ src/graphics/Renderer.cpp
@@ -10,6 +10,12 @@ namespace Graphics {
 
     void Renderer::render() {
         // TODO: actually render
+        sf::RectangleShape shape;
+        shape.setFillColor(sf::Color::Black);
+        shape.setPosition(100, 100);
+        shape.setSize(sf::Vector2f(250, 250));
+
+        _window->draw(shape);
     }
 
 } // Graphics
